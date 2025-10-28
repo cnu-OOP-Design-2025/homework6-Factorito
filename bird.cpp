a10eef4465f787a2217228447a6397701e61db19
Factorito <redbulue0509@gmail.com>
2025-10-28 12:52:06 +0900

    과제 제출

--- bird.cpp
+++ bird.cpp
@@ -1,3 +1,58 @@
 #include <iostream>
 #include "bird.h"
 
+class Duck : public Bird
+{
+    public:
+    Duck(): Bird("Duck") {
+         cry();
+    }
+    virtual ~Duck() {
+        cry();
+     }
+
+     virtual void cry(){
+        std :: cout << "Quack!" << std:: endl;
+     }
+
+    };
+
+class Penguin : public Bird{
+    public:
+    Penguin(): Bird("Penguin") {
+         cry();
+    }
+
+    virtual void fly() {
+        std::cout << "This bird can't fly." << std::endl;
+    }
+
+    virtual ~Penguin() {
+        cry();
+     }
+
+     virtual void cry(){
+        std :: cout << "Squawk!" << std::endl;
+     }
+};
+
+class Eagle : public Bird{
+    public:
+    Eagle(): Bird("Eagle") {
+         cry();
+    }
+
+     virtual void swim() {
+        std::cout << "This bird can't swim." << std::endl;
+    }
+
+    virtual ~Eagle() {
+        cry();
+        
+     }
+    virtual void cry(){
+            std::cout << "Screech!" << std::endl;
+        }
+};
+
+
